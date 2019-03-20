0c273c2cbfcfa8b6e1fa76cd2bf8f19df9c33a67
Uli <post2uli@gmx.de>
2019-03-20 15:58:14 +0100

    ricicle view

--- app/src/main/java/de/auli/recyclerview/MainActivity.java
+++ app/src/main/java/de/auli/recyclerview/MainActivity.java
@@ -1,10 +1,18 @@
 package de.auli.recyclerview;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,14 +41,42 @@ public class MainActivity extends AppCompatActivity {
                new Person("Hein Mück", "Bremen"),
                new Person("Resi", "München")));
 
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+            }
+
+            @Override
+            public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+                super.onDrawOver(c, parent, state);
+                RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
+                Paint paintCyan = new Paint(Paint.ANTI_ALIAS_FLAG);
+                paintCyan.setColor(Color.CYAN);
+                paintCyan.setStyle(Paint.Style.STROKE);
+                paintCyan.setStrokeWidth(5);
+                for(int i=0;i<parent.getChildCount();i++){
+                    View child = parent.getChildAt(i);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        c.drawRoundRect(layoutManager.getDecoratedLeft(child)+10,
+                                layoutManager.getDecoratedTop(child)+ 10,
+                                layoutManager.getDecoratedRight(child)-10,
+                                layoutManager.getDecoratedBottom(child)-10,15,15,paintCyan);
+                    }
+                }
+            }
+        });
+
+
         recyclerView = findViewById(R.id.id_recyclerview);
-        recyclerViewLayoutManager = new LinearLayoutManager(this);
+        recyclerViewLayoutManager = new GridLayoutManager(this, 3);
         recyclerViewAdapter = new MyRecyclerViewAdapter();
         recyclerView.setLayoutManager(recyclerViewLayoutManager);
         recyclerView.setAdapter(recyclerViewAdapter);
 
-        dividerItemDecoration= new DividerItemDecoration(recyclerView.getContext(), 1);
-        recyclerView.addItemDecoration(dividerItemDecoration);
+        //dividerItemDecoration= new DividerItemDecoration(recyclerView.getContext(), 1);
 
     }
+
+
 }
